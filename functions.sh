03864dd94cdbcab05453f37872c31e7c4f46af41
Alexandre Mély <alexandre.mely@gmail.com>
2017-04-28 18:18:12 +0200

    bug fix

--- functions.sh
+++ functions.sh
@@ -1,8 +1,6 @@
 jv_pg_ui_start () {
     jv_debug "Starting User Interface on http://$jv_ip:$jv_pg_ui_port"
-    local previous_dir="$(pwd)"
-    local current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    cd $current_dir/../
+    cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     nohup python -m SimpleHTTPServer $jv_pg_ui_port >/dev/null 2>/dev/stdout & 
-    cd $previous_dir
+    cd $jv_dir
 }
