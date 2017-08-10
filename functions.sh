70de275ce5b3c314437575bcee5152a9746a6ac5
Emilien Devos <unixfox@users.noreply.github.com>
2017-08-10 20:49:34 +0200

    Change the startup command from python to python2

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@ jv_pg_ui_start () {
     if jv_plugin_is_enabled "jarvis-api"; then
         jv_debug "Starting User Interface on http://$jv_ip:$jv_pg_ui_port"
         cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-        nohup python -m SimpleHTTPServer $jv_pg_ui_port >/dev/null 2>/dev/stdout & 
+        nohup python2 -m SimpleHTTPServer $jv_pg_ui_port >/dev/null 2>/dev/stdout & 
         cd $jv_dir
     else
         jv_error "ERROR: Jarvis-UI requires Jarvis-API plugin to be installed and enabled"
